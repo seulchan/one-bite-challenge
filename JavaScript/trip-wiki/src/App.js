25347b41b09999f7be56e494a894dabd72ce04f8
Chan <seulchan07@gmail.com>
2025-12-24 09:56:27 +0700

    JS Day13 미션1 구현

--- JavaScript/trip-wiki/src/App.js
+++ JavaScript/trip-wiki/src/App.js
@@ -5,16 +5,93 @@ import CityDetail from "./components/CityDetail.js";
 import { request } from "./api.js";
 
 export default function App($app) {
+  const getSortBy = () => {
+    if (window.location.search) {
+      return window.location.search.split("sort=")[1].split("&")[0];
+    }
+    return "total";
+  };
+  const getSearchWord = () => {
+    if (window.location.search && window.location.search.includes("search=")) {
+      return window.location.search.split("search=")[1];
+    }
+    return "";
+  };
+
   this.state = {
     startIdx: 0,
-    sortBy: "",
-    searchWord: "",
+    sortBy: getSortBy(),
+    searchWord: getSearchWord(),
     region: "",
     cities: "",
   };
 
-  const header = new Header();
-  const regionList = new RegionList();
+  const header = new Header({
+    $app,
+    initialState: {
+      sortBy: this.state.sortBy,
+      searchWord: this.state.searchWord,
+    },
+    handleSortChange: async (sortBy) => {
+      const pageUrl = `/${this.state.region}?sort=${sortBy}`;
+      history.pushState(
+        null,
+        null,
+        this.state.searchWord
+          ? pageUrl + `&search=${this.state.searchWord}`
+          : pageUrl,
+      );
+      const cities = await request(
+        0,
+        this.state.region,
+        sortBy,
+        this.state.searchWord,
+      );
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        sortBy: sortBy,
+        cities: cities,
+      });
+    },
+    handleSearch: async (searchWord) => {
+      history.pushState(
+        null,
+        null,
+        `/${this.state.region}?sort=${this.state.sortBy}&search=${searchWord}`,
+      );
+      const cities = await request(
+        0,
+        this.state.region,
+        this.state.sortBy,
+        searchWord,
+      );
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        cities: cities,
+        searchWord: searchWord,
+      });
+    },
+  });
+
+  const regionList = new RegionList({
+    $app,
+    initialState: this.state.region,
+    handleRegion: async (region) => {
+      history.pushState(null, null, `/${region}?sort=total`);
+      const cities = await request(0, region, "total");
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        sortBy: "total",
+        region: region,
+        searchWord: "",
+        cities: cities,
+      });
+    },
+  });
+
   const cityList = new CityList({
     $app,
     initialState: this.state.cities,
@@ -40,7 +117,36 @@ export default function App($app) {
   this.setState = (newState) => {
     this.state = newState;
     cityList.setState(this.state.cities);
+    header.setState({
+      sortBy: this.state.sortBy,
+      searchWord: this.state.searchWord,
+    });
+    regionList.setState(this.state.region);
   };
+
+  window.addEventListener("popstate", async () => {
+    const urlPath = window.location.pathname;
+
+    const prevRegion = urlPath.replace("/", "");
+    const prevSortBy = getSortBy();
+    const prevSearchWord = getSearchWord();
+    const prevStartIdx = 0;
+    const prevCities = await request(
+      prevStartIdx,
+      prevRegion,
+      prevSortBy,
+      prevSearchWord,
+    );
+
+    this.setState({
+      ...this.state,
+      startIdx: prevStartIdx,
+      sortBy: prevSortBy,
+      region: prevRegion,
+      searchWord: prevSearchWord,
+      cities: prevCities,
+    });
+  });
 
   const init = async () => {
     const cities = await request(
